2713767efb7246632d75b60885e4a83f7cabfcd9
JavadJan <92234079+JavadJan@users.noreply.github.com>
2023-12-17 19:36:33 +0100

    edited the opacity

--- app/page.jsx
+++ app/page.jsx
@@ -35,26 +35,26 @@ const Home = () => {
                     :
                     <>
                         {/* bg-[#160F30] */}
-                        <section className={`px-28 max-lg:px-[3%] w-[100%]  z-50 sticky top-0 bg-[#001524] ${active ? 'scroll_active lg:bg-white text-black ' : 'text-white'}`}>
+                        <section className={`snap-start px-28 max-lg:px-[3%] w-[100%]  z-50 sticky top-0 bg-[#001524] ${active ? 'scroll_active lg:bg-white text-black ' : 'text-white'}`}>
                             <Nav />
                         </section>
-                        <section id="home" className="bg-[#001524] relative top-0 text-white-400 overflow-hidden">
+                        <section id="home" className="snap-start bg-[#001524] relative top-0 text-white-400 overflow-hidden">
                             <Profile />
                         </section>
-                        <section id="skills" className=' section padding bg-[#F3FDE8] h-[55vh] max-sm:h-[45vh] overflow-hidden'>
+                        <section id="skills" className='snap-start section padding bg-[#F3FDE8] h-[55vh] max-sm:h-[45vh] overflow-hidden'>
                             <Skills />
                         </section>
                         <section id="project" className='snap-start h-auto bg-[#F6F6F6] sm:px-16 px-3 sm:py-24 py-6'>
                             <Projects />
                         </section>
-                        <section id="contact-me" className="max-sm:px-0 px-[3%] bg-planet h-[content] py-28">
+                        <section id="contact-me" className="snap-start max-sm:px-0 px-[3%] bg-planet h-[content] py-28">
                             <ContactMe />
                         </section>
-                        <section id="about-me" className=" section padding">
+                        <section id="about-me" className="snap-start section padding">
                             <AboutMe />
                         </section>
 
-                        <section id="footer" className=' section padding bg-black padding-t padding-x pb-8'>
+                        <section id="footer" className='snap-start section padding bg-black padding-t padding-x pb-8'>
                             <Footer />
                         </section>
                     </>
